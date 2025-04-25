fcf0eceadb060ff51da8e6ef575abdc69827a319
T1ckL35 <simon.mann@gmail.com>
2025-04-25 11:37:25 +0100

    pushing changes

--- .github/scripts/test.py
+++ .github/scripts/test.py
@@ -1,6 +1,68 @@
+"""
+Todo...
+    file.py -f "modules/module1/main.tf modules/module2/main.tf"
+"""
+
+import argparse
+import logging
 import os
 import sys
 
-print(os.getcwd())
-print("Hello world from script file!")
-sys.exit()
+class PackageModule:
+
+    logfile_name = "mylogfile"
+    args = None
+
+    def __init__(self, options=None):
+        # Sets up normal file logging (DEBUG) and add additional logging formatting
+        logging.basicConfig(filename=self.logfile_name,level=logging.DEBUG, format='%(asctime)s %(name)s %(levelname)s: %(message)s')
+        logging.info('PackageModule initialising...')
+
+    def configure(self):
+        """
+        Retrieves any defined input arguments
+        """
+        logging.info('PackageModule configuring input...')
+        parser = argparse.ArgumentParser()
+        # required = parser.add_argument_group('required arguments')
+        parser.add_argument("-f", "--files", help="string of space separated file paths that have been updated.")
+        self.args = parser.parse_args()
+        logging.info(f"Input args: {self.args}")
+
+    def output_logging(self):
+        """
+        Add an additional (INFO) logger (to the file logger) to also output information to the screen. Intended for terminal, AWS Lambda functions or similar.
+        :return:
+        """
+        root_logger = logging.getLogger()
+        output_logger = logging.StreamHandler(sys.stdout)
+        output_logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        output_logger.setFormatter(formatter)
+        root_logger.addHandler(output_logger)
+
+    def run(self):
+        logging.info('PackageModule running...')
+        self.configure()
+        files_string = self.args.files
+        files_list = files_string.split()
+        # go through each filepath and get the module name
+        modules_list = []
+        for file in files_list:
+            module_name = file.split(os.path.sep)[-2]
+            if module_name not in modules_list:
+                modules_list.append(module_name)
+        print(modules_list)
+
+
+if __name__ == "__main__":
+    try:
+        app = PackageModule()
+        app.output_logging()
+        app.run()
+    except Exception as e:
+        print("EXCEPTION ENCOUNTERED:")
+        print(e)
+        logging.exception(e, exc_info=True)
+        sys.exit(1)
+
